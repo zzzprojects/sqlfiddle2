4efacbe26642af5eb0d95c2abf6e6ac888107caf
jakefeasel <jfeasel@gmail.com>
2014-05-08 18:58:14 -0700

    New rules for anonymous access to various endpoints

--- src/main/resources/script/access.js
+++ src/main/resources/script/access.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
  * Copyright (c) 2011-2012 ForgeRock AS. All rights reserved.
@@ -23,8 +23,8 @@
  */
 
 
-// A configuration for allowed HTTP requests. Each entry in the configuration contains a pattern 
-// to match against the incoming request ID and, in the event of a match, the associated roles, 
+// A configuration for allowed HTTP requests. Each entry in the configuration contains a pattern
+// to match against the incoming request ID and, in the event of a match, the associated roles,
 // methods, and actions that are allowed for requests on that particular pattern.
 //
 // pattern:  A pattern to match against an incoming request's resource ID
@@ -37,23 +37,41 @@
 // A single '*' character indicates all possible values.  With patterns ending in "/*", the "*"
 // acts as a wild card to indicate the pattern accepts all resource IDs "below" the specified
 // pattern (prefix).  For example the pattern "managed/*" would match "managed/user" or anything
-// starting with "managed/".  Note: it would not match "managed", which would need to have its 
+// starting with "managed/".  Note: it would not match "managed", which would need to have its
 // own entry in the config.
 
-/*jslint vars:true*/ 
+/*jslint vars:true*/
 
-var httpAccessConfig = 
-{ 
+var httpAccessConfig =
+{
     "configs" : [
         // Anyone can read from these endpoints
-        {  
-           "pattern"    : "info/*",
+        {
+           "pattern"    : "system/fiddles/db_types",
+           "roles"      : "*",
+           "methods"    : "query",
+           "actions"    : "*"
+        },
+        {
+           "pattern"    : "endpoint/loadContent/*",
            "roles"      : "*",
            "methods"    : "read",
            "actions"    : "*"
         },
+        {
+           "pattern"    : "endpoint/createSchema",
+           "roles"      : "*",
+           "methods"    : "create",
+           "actions"    : "*"
+        },
+        {
+           "pattern"    : "endpoint/executeQuery",
+           "roles"      : "*",
+           "methods"    : "action",
+           "actions"    : "execute"
+        },
         // openidm-admin can request nearly anything (some exceptions being a few system endpoints)
-        {  
+        {
             "pattern"   : "*",
             "roles"     : "openidm-admin",
             "methods"   : "*", // default to all methods allowed
@@ -62,7 +80,7 @@ var httpAccessConfig =
             "excludePatterns": "system/*"
         },
         // additional rules for openidm-admin that selectively enable certain parts of system/
-        {  
+        {
             "pattern"   : "system/*",
             "roles"     : "openidm-admin",
             "methods"   : "create,read,update,delete,patch,query", // restrictions on 'action'
@@ -70,14 +88,13 @@ var httpAccessConfig =
             "customAuthz" : "disallowQueryExpression()"
         },
         // Note that these actions are available directly on system as well
-        {  
+        {
             "pattern"   : "system/*",
             "roles"     : "openidm-admin",
             "methods"   : "action",
             "actions"   : "test,testConfig,createconfiguration,liveSync"
         }
-    ] 
+    ]
 };
 
 // Additional custom authorization functions go here
-
