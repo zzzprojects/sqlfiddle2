17f5ef6e17bc76b34731915aa370785f91cc0cb9
jakefeasel <jfeasel@gmail.com>
2014-11-02 01:23:34 -0800

    Configuration support for MS SQL Server and Oracle JDBC drivers

--- vagrant_scripts/idm_bootstrap.sh
+++ vagrant_scripts/idm_bootstrap.sh
@@ -15,6 +15,11 @@ apt-get --yes --force-yes install openjdk-7-jdk maven npm varnish
 ln -s /usr/bin/nodejs /usr/bin/node
 npm install -g grunt-cli
 
+# If you want to enable Oracle support, manually download ojdbc6.jar and put it in the root folder (up one level from here)
+# Download it from here: http://www.oracle.com/technetwork/database/enterprise-edition/jdbc-112010-090769.html
+# Afterwards, uncomment the below line as well as the dependency in ../pom.xml
+#mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.4 -Dpackaging=jar -Dfile=/vagrant/ojdbc6.jar
+
 cd /vagrant
 mvn clean install
 npm install
