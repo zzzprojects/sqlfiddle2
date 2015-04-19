e293c125f954aaecab7ef2aa8c9a7d45b028108d
jakefeasel <jfeasel@gmail.com>
2015-04-19 13:07:42 -0700

    Fixes bug with paths in idm_aws.sh

--- vagrant_scripts/idm_aws.sh
+++ vagrant_scripts/idm_aws.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+cd /vagrant
+
 grunt sync less requirejs
 update-rc.d openidm defaults
 service varnish restart
 
-if [ -d "./openvpn" ]
+if [ -d "/vagrant/vagrant_scripts/openvpn" ]
 then
 
     apt-get --yes --force-yes install openvpn
-    cp ./openvpn/* /etc/openvpn
+    cp /vagrant/vagrant_scripts/openvpn/* /etc/openvpn
 
     echo "192.168.199.2   POSTGRESQL84_HOST" >> /etc/hosts
     echo "192.168.199.3   POSTGRESQL92_HOST" >> /etc/hosts
