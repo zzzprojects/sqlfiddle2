7d1bf51439b1fb1a35557ac9b696426cbc2df9e3
jakefeasel <jfeasel@gmail.com>
2015-05-20 08:50:15 -0700

    Fixes paths for my.cnf in mysql55

--- vagrant_scripts/my55_bootstrap.sh
+++ vagrant_scripts/my55_bootstrap.sh
@@ -4,7 +4,7 @@ apt-get --yes update
 apt-get --yes upgrade
 
 mkdir -p /etc/mysql/conf.d
-cp /vagrant/src/main/resources/db/mysql/my.cnf /etc/mysql/conf.d
+cp /vagrant/src/main/resources/db/mysql/my.cnf /etc/mysql/my.cnf
 
 echo mysql-server-5.5 mysql-server/root_password password password | sudo debconf-set-selections
 echo mysql-server-5.5 mysql-server/root_password_again password password | sudo debconf-set-selections
