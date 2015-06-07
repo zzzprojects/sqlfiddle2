6e3823df5e98e85883692bcfee9b4330b7673ba2
jakefeasel <jfeasel@gmail.com>
2015-06-07 13:42:23 -0700

    Uses the same my.cnf settings for mysql55 as is used for mysql56

--- vagrant_scripts/my55_bootstrap.sh
+++ vagrant_scripts/my55_bootstrap.sh
@@ -4,11 +4,14 @@ apt-get --yes update
 apt-get --yes upgrade
 
 mkdir -p /etc/mysql/conf.d
-cp /vagrant/src/main/resources/db/mysql/my.cnf /etc/mysql/my.cnf
 
 echo mysql-server-5.5 mysql-server/root_password password password | sudo debconf-set-selections
 echo mysql-server-5.5 mysql-server/root_password_again password password | sudo debconf-set-selections
 
 apt-get --yes install mysql-server-5.5 mysql-client-5.5
+
+cat /vagrant/src/main/resources/db/mysql/my.cnf >> /etc/mysql/my.cnf
+
+service mysql restart
 
 echo "grant all privileges on *.* to root@'%' identified by 'password' with grant option;" | /usr/bin/mysql -u root -ppassword
